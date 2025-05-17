6e84ac506d70aed1948445e5f1926130853035b2
kili196 <kilian.baier16@gmail.com>
2025-05-17 15:01:08 +0200

    put label links in array

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -3,47 +3,29 @@ import { NavLink, Link } from "react-router";
 export default function Sidebar() {
   const navLinkStyling = "font-medium text-white text-xl p-2 rounded-lg";
 
+  const navLinks = [
+    { to: "/", label: "Home" },
+    { to: "json-converter", label: "JSON Converter" },
+    { to: "cmd-cheatsheet", label: "CMD Cheatsheet" },
+    { to: "color-picker", label: "Color Picker" },
+  ];
+
   return (
     <>
-      <div className="bg-blue-500 xl:w-96">
+      <div className="bg-blue-500 xl:w-96 shadow-[6px_0_10px_-2px_rgba(0,0,0,0.2)]">
         {" "}
         <ul className="flex flex-col gap-4 p-5">
-          <NavLink
-            className={({ isActive }) =>
-              isActive ? `bg-blue-400 ${navLinkStyling}` : navLinkStyling
-            }
-            to={"/"}
-          >
-            {" "}
-            Home
-          </NavLink>
-          <NavLink
-            className={({ isActive }) =>
-              isActive ? `bg-blue-400 ${navLinkStyling}` : navLinkStyling
-            }
-            to={"json-converter"}
-          >
-            {" "}
-            JSON Converter
-          </NavLink>
-          <NavLink
-            className={({ isActive }) =>
-              isActive ? `bg-blue-400 ${navLinkStyling}` : navLinkStyling
-            }
-            to={"cmd-cheatsheet"}
-          >
-            {" "}
-            CMD Cheatsheet
-          </NavLink>
-          <NavLink
-            className={({ isActive }) =>
-              isActive ? `bg-blue-400 ${navLinkStyling}` : navLinkStyling
-            }
-            to={"color-picker"}
-          >
-            {" "}
-            Color Picker
-          </NavLink>
+          {navLinks.map((element) => (
+            <NavLink
+              to={element.to}
+              className={({ isActive }) =>
+                isActive ? `bg-blue-400 ${navLinkStyling}` : navLinkStyling
+              }
+            >
+              {" "}
+              {element.label}
+            </NavLink>
+          ))}
         </ul>
       </div>
     </>
