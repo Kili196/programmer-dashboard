90dd06ed46f627240179d27776f0bcaafeb3fa19
kili196 <kilian.baier16@gmail.com>
2025-05-17 14:02:17 +0200

    add sidebar navlink
    add routes and children
    
    add active link to navlink

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,7 +1,51 @@
+import { NavLink, Link } from "react-router";
+
 export default function Sidebar() {
+  const navLinkStyling = "font-medium text-white text-xl p-1";
+
   return (
     <>
-      <div className="bg-blue-500 xl:w-96"> Sidebar</div>
+      <div className="bg-blue-500 xl:w-96">
+        {" "}
+        <ul className="flex flex-col gap-4 p-7">
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? `bg-blue-400 ${navLinkStyling}` : navLinkStyling
+            }
+            to={"/"}
+          >
+            {" "}
+            Home
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? `bg-blue-400 ${navLinkStyling}` : navLinkStyling
+            }
+            to={"json-converter"}
+          >
+            {" "}
+            JSON Converter
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? `bg-blue-400 ${navLinkStyling}` : navLinkStyling
+            }
+            to={"cmd-cheatsheet"}
+          >
+            {" "}
+            CMD Cheatsheet
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? `bg-blue-400 ${navLinkStyling}` : navLinkStyling
+            }
+            to={"color-picker"}
+          >
+            {" "}
+            Color Picker
+          </NavLink>
+        </ul>
+      </div>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,30 @@ import "./index.css";
 
 import Sidebar from "./Sidebar.jsx";
 import Layout from "./Layout.jsx";
+import { BrowserRouter, Outlet, RouterProvider } from "react-router";
+import { createBrowserRouter } from "react-router";
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <Layout>
+        {<Sidebar />}
+        <div className="flex-1">
+          <Outlet />
+        </div>
+      </Layout>
+    ),
+    children: [
+      { path: "json-converter", element: <div> test </div> },
+      { path: "cmd-cheatsheet", element: <div> test </div> },
+      { path: "color-picker", element: <div> test </div> },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Layout>
-      {" "}
-      {<Sidebar />} <div className="flex-1"> hello</div>
-    </Layout>
+    <RouterProvider router={routes} />
   </StrictMode>
 );
